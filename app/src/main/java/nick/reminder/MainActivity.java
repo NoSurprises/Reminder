2d2bc296b4a06bdc27f4d7ae126d86b6ea58373c
NoSurprises <helloma@yandex.ru>
2018-01-10 17:51:35 +0300

    Added toolbar and search menu item

--- app/src/main/java/nick/reminder/MainActivity.java
+++ app/src/main/java/nick/reminder/MainActivity.java
@@ -1,13 +1,37 @@
 package nick.reminder;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MainActivity extends Activity {
+
+
+    @BindView(R.id.toolbar) Toolbar toolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        initializeToolbar();
+    }
+
+    private void initializeToolbar() {
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolBar.inflateMenu(R.menu.menu);
     }
 }
